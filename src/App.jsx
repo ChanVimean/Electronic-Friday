d71c4a22cc4d6d76704db62a09b53864c57eb62c
ChanVimean <mrrmeanvlogger@gmail.com>
2025-09-05 14:51:26 +0700

    Add Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 // Shortcut: rafce
+import { Route, Routes } from "react-router-dom"
+import Footer from "./components/Footer"
+import NavBar from "./components/NavBar"
+import Home from "./page/Home"
+import About from "./page/About"
+import Contact from "./page/Contact"
 
 const App = () => {
+
   return (
-    <div>
-      <h1>Hosting on Vercel</h1>
-    </div>
+    <>
+
+      <nav>
+        <NavBar />
+      </nav>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />          
+        </Routes>
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+
+    </>
   )
 }
 
